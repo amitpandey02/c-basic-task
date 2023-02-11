4400a93d4d5f7ee5ae63e6d086e0c2c8d872fe86
amitpandey02 <124866678+amitpandey02@users.noreply.github.com>
2023-02-11 17:34:13 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public delegate int MyDelegate(int num, int num1);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate obj = (a,b) =>
+            {
+                int c = a + b;
+                return c;
+               
+                Console.WriteLine(c);
+            };
+           int num = obj.Invoke(10,20);
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ StringInterpolationTask2/StringInterpolationTask2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolationTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Amit";
+            string a = "pandey";
+            Console.WriteLine("hello "+s+" "+a); 
+
+            //String Interpolation
+            Console.WriteLine($"hello {s} {a}");
+
+
+            Stopwatch sw1 = new Stopwatch();
+
+            sw1.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                s = s + i;
+            }
+            sw1.Stop();
+            StringBuilder sb = new StringBuilder("Hello");
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.Append(i);
+            }
+            sw2.Stop();
+            Console.WriteLine("Time taken by string :" + sw1.ElapsedMilliseconds);
+            Console.WriteLine("Time taken by stringbuilder :" + sw2.ElapsedMilliseconds);
+        }
+    }
+}
