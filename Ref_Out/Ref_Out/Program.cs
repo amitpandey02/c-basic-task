8558664ba88d9dd1bd3e8d1cfc257623e245f9a9
amitpandey02 <124866678+amitpandey02@users.noreply.github.com>
2023-02-13 14:31:37 +0530

    Add files via upload

---
+++ Ref_Out/Ref_Out/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_Out
+{
+  public class Program
+    {
+        /*
+        static void PassByRef(ref int a)
+        {
+            a = a + 10;
+            Console.WriteLine("value is:"+a);
+        }
+        static void Main(string[] args)
+        {
+            int value = 5;
+
+            PassByRef(ref value);
+            Console.WriteLine(value);
+        }
+        */
+
+        static void PassByOut(out int a)
+        {
+            a = 20;
+            Console.WriteLine("value is "+a);
+        }
+        static void Main(string[] args)
+        {
+            int value;
+            PassByOut(out value);
+            Console.WriteLine(value);
+
+        }
+    }
+}
