c271a900d7ca9d7b44482fa1b2ac30d13d999c35
amitpandey02 <124866678+amitpandey02@users.noreply.github.com>
2023-02-13 17:25:44 +0530

    Add files via upload

---
+++ Var_Dynamic_Object/Var_Dynamic_Object/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_Dynamic_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = "Amit";
+            //var v = 5;
+            // can not intialize twice
+
+            dynamic d = 5;
+            d = "pandey";
+
+            object o = 5;
+            o = "pandey1";
+
+            // OBJECT
+            //String s = o;
+            // we need to explicitly conver it
+            String s=o.ToString();
+
+            //Dynamic
+            //implicitly converted
+            String s1 = d;
+
+            //Var
+            string s2 = v;
+
+            Console.WriteLine("var :"+v);
+            Console.WriteLine("dynamic :"+d);
+            Console.WriteLine("object :"+o);
+            Console.WriteLine(s);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+        }
+    }
+}
